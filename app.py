87da759a54293a61663c6a65dd1294f992038f45
Niraj Acharya <62750137+ayrahca@users.noreply.github.com>
2024-11-15 08:56:36 +0545

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def convert_bs_to_ad():
         return render_template('index.html', error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=Fase,host='0.0.0.0')
